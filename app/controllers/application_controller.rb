6129ddb4f5ad66f1a6954258941da7f1bf32bc74
Evan McCullough <evan.mccullough@modolabs.com>
2020-08-10 07:09:10 -0400

    add name to admin users (#10)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     helper_method :ozToLbs, :getBabyWinner
     def after_sign_in_path_for(resource)
       stored_location_for(resource) || babies_path
@@ -16,4 +22,10 @@ class ApplicationController < ActionController::Base
         pool = Pool.where(baby_id: baby_id, winner: true).limit(1).first
         return pool
     end
+
+    protected
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+        end
 end
