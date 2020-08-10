0d10cfd4d76ce8562b5cd3b3aaf62831e1b2465c
Evan McCullough <evan.mccullough@modolabs.com>
2020-08-10 06:56:31 -0400

    added edit my account and fixed up some devise ui (#8)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,6 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :pool, dependent: :delete_all
+  has_many :baby, dependent: :delete_all
 end
